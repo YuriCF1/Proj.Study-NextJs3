880298a1215c4f42068288960320cccbb7f56f46
Yuri Cruz <yuricruzf@gmail.com>
2023-09-29 09:56:24 -0300

    Example of fixing bug of dynamic pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,9 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
+// export const dynamic = 'force-dynamic'
+// export const revalidate = 0
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
