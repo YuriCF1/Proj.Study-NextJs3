2e8d57d8bb60e4211bfda1a9475d150422ceda53
Yuri Cruz <yuricruzf@gmail.com>
2023-09-29 09:57:01 -0300

    Organizing components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,37 +4,24 @@ Por padrão, o Next sempre é estático, então uma requisição direta só ser�
 */
 
 import Link from "next/link";
+import { User } from "./components/User";
+import { Repo } from "./components/Repo";
+import Cookies from "./components/Cookies";
 // export const revalidate = 5; //Revalidando todas as requisições da página a cada 5 segundos.
 
 const Page = async () => {
-  const res = await fetch("http://api.github.com/users/YuriCF1", {
-    next: {
-      revalidate: 30, //Revalidando apenas essa chamada, a cada trinta segundos
-      // cache: 'no-store' //O padrão é 'force-storage', para que todos os usuários recebam a mesma resposta. 
-    },
-  });
-  const user = await res.json();
-
-  type Time = {
-    datetime: string
-  }
-
-  const contando = async function getTime(): Promise<Time> {
-    const time = await fetch('http:worldtimeapi.org/api/timezone/America/Chicago', {
-      cache: 'no-store' //O padrão é 'force-storage', para que todos os usuários recebam a mesma resposta. 
-    })
-    return time.json()
-  }
-  const [tempo] = await Promise.all([contando()])
-
+  //Dica: Caso eu pricesse fazer mais de um fetch, que sejam independentes, usar sempre o promisse all
+  // const [res1, res2] = await Promise.all([
+  //   fetch(''), fetch('')]
+  // )
   return (
     <div>
-      <h1>Hello world!</h1>
-      <pre>{JSON.stringify(user, null, 2)}</pre>
-      <p>{tempo.datetime}</p>
-      <Link href="/dashboard">Dashboard</Link>
+      <h1>Home</h1>
+      <Cookies />
+      <User />
+      {/* <Repo /> */}
     </div>
-  );
+  )
 };
 
 export default Page;
@@ -55,101 +42,3 @@ export default Page;
   }
   const [tempo] = await Promise.all([contando()])
 */
-
-
-
-// import Image from 'next/image'
-// import styles from './page.module.css'
-
-// export default function Home() {
-//   return (
-//     <main className={styles.main}>
-//       <div className={styles.description}>
-//         <p>
-//           Get started by editing&nbsp;
-//           <code className={styles.code}>src/app/page.tsx</code>
-//         </p>
-//         <div>
-//           <a
-//             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-//             target="_blank"
-//             rel="noopener noreferrer"
-//           >
-//             By{' '}
-//             <Image
-//               src="/vercel.svg"
-//               alt="Vercel Logo"
-//               className={styles.vercelLogo}
-//               width={100}
-//               height={24}
-//               priority
-//             />
-//           </a>
-//         </div>
-//       </div>
-
-//       <div className={styles.center}>
-//         <Image
-//           className={styles.logo}
-//           src="/next.svg"
-//           alt="Next.js Logo"
-//           width={180}
-//           height={37}
-//           priority
-//         />
-//       </div>
-
-//       <div className={styles.grid}>
-//         <a
-//           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-//           className={styles.card}
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           <h2>
-//             Docs <span>-&gt;</span>
-//           </h2>
-//           <p>Find in-depth information about Next.js features and API.</p>
-//         </a>
-
-//         <a
-//           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-//           className={styles.card}
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           <h2>
-//             Learn <span>-&gt;</span>
-//           </h2>
-//           <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-//         </a>
-
-//         <a
-//           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-//           className={styles.card}
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           <h2>
-//             Templates <span>-&gt;</span>
-//           </h2>
-//           <p>Explore the Next.js 13 playground.</p>
-//         </a>
-
-//         <a
-//           href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-//           className={styles.card}
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           <h2>
-//             Deploy <span>-&gt;</span>
-//           </h2>
-//           <p>
-//             Instantly deploy your Next.js site to a shareable URL with Vercel.
-//           </p>
-//         </a>
-//       </div>
-//     </main>
-//   )
-// }
