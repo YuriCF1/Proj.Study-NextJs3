91b3bb0c3d06aa50b0b4bbf87f1cfc45a6b91aec
Yuri Cruz <yuricruzf@gmail.com>
2023-09-30 00:45:03 -0300

    Testing fetch in components

--- src/app/components/Repo.tsx
+++ src/app/components/Repo.tsx
@@ -1,5 +1,6 @@
 export async function Repo() {
-    const res = await fetch("http://api.github.com/users/YuriCF1/repos", {
+    // const res = await fetch("http://api.github.com/users/YuriCF1/repos", {
+    const res = await fetch("https://jsonplaceholder.typicode.com/todos/1", {
         next: {
             revalidate: 30, //Revalidando apenas essa chamada, a cada trinta segundos
             // cache: 'no-store' //O padrão é 'force-storage', para que todos os usuários recebam a mesma resposta. 
--- src/app/components/User.tsx
+++ src/app/components/User.tsx
@@ -1,7 +1,12 @@
+import Cookies from "./Cookies";
 import { Repo } from "./Repo";
 
 export async function User() {
-    const res = await fetch("http://api.github.com/users/YuriCF1", { //OBS: O Next consegue reconhecer quando a mesma fetch está sendo executada diversas vezes. Executa uma e destribui os dados par aos componentes filhos
+    await new Promise((resolve) => {
+        setTimeout((resolve), 3000);
+    })
+    // const res = await fetch("http://api.github.com/users/YuriCF1", { //OBS: O Next consegue reconhecer quando a mesma fetch está sendo executada diversas vezes. Executa uma e destribui os dados par aos componentes filhos
+    const res = await fetch("https://jsonplaceholder.typicode.com/todos/1", { //OBS: O Next consegue reconhecer quando a mesma fetch está sendo executada diversas vezes. Executa uma e destribui os dados par aos componentes filhos
         next: {
             revalidate: 30, //Revalidando apenas essa chamada, a cada trinta segundos
             // cache: 'no-store' //O padrão é 'force-storage', para que todos os usuários recebam a mesma resposta. 
@@ -11,6 +16,7 @@ export async function User() {
     return (
         <div>
             <h1>User!</h1>
+            <Cookies />
             <pre>{JSON.stringify(user, null, 2)}</pre>
             <div>
                 <Repo />
