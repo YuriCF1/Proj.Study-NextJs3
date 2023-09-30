c23891234d6995af56e9ae58c3dd855f721aa651
Yuri Cruz <yuricruzf@gmail.com>
2023-09-30 00:44:42 -0300

    Getting cookies

--- src/app/components/Cookies.tsx
+++ src/app/components/Cookies.tsx
@@ -1,13 +1,19 @@
-import { cookies, headers } from "next/headers"
+// 'use server'
+
+// import { cookies, headers } from "next/headers"
+import { useCookies } from 'next-client-cookies';
+
 export default async function Cookies() {
-    const userCokies = cookies()
-    const userHeaders = headers()
+    // const userCokies = cookies()
+    const cookies = useCookies();
+    
+    // const userHeaders = headers()
 
     return (
         <div>
             <h1>Cookies e Headers</h1>
-            {JSON.stringify(userCokies.get('Auth'), null, 2)}
-            {JSON.stringify(userHeaders.entries(), null, 2)}
+            {JSON.stringify(cookies.get('Auth'), null, 2)}
+            {/* {JSON.stringify(userHeaders.entries(), null, 2)} */}
         </div>
     )
 }
